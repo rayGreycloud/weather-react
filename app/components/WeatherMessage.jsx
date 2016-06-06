f5949374ded71004c79768266d541cec88ee1d7c
rayGreycloud <rayGreycloud@gmail.com>
2016-06-06 15:53:29 -0700

    add form and message components

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var WeatherMessage = React.createClass({
+  render: function() {
+
+    return (
+      <div>
+        <h3>It's sunny in Portland</h3>
+      </div>
+    );
+  }
+});
+
+module.exports = WeatherMessage;
