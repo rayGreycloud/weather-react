708e15144f7ced64448e3cffee69f0c216971138
rayGreycloud <rayGreycloud@gmail.com>
2016-06-07 14:32:46 -0700

    add openWeather api call

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,17 +1,35 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
+  getInitialState: function(){
+    return {
+        location: "Portland",
+        temp: 80
+    }
+  },
   handleSearch: function(location) {
-    alert(location);
+    var that = this;
+
+    openWeatherMap.getTemp(location).then(function(temp) {
+      that.setState({
+        location: location,
+        temp: temp
+      });
+    }, function(errorMessage) {
+      alert(errorMessage);
+    });
   },
   render: function() {
+    var {temp, location} = this.state;
+
     return (
       <div>
         <h3>Get Weather</h3>
         <WeatherForm onSearch={this.handleSearch}/>
-        <WeatherMessage/>
+        <WeatherMessage temp={temp} location={location}/>
       </div>
     )
   }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,10 +2,11 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
   render: function() {
+    var {temp, location} = this.props;
 
     return (
       <div>
-        <h3>It's sunny in Portland</h3>
+        <h3>It's {temp} in {location}</h3>
       </div>
     );
   }
