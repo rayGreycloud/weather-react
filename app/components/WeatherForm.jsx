25fff1768d6f7a208d4559ed751c6914714eb745
rayGreycloud <rayGreycloud@gmail.com>
2016-06-07 12:54:02 -0700

    Revise package.json and webpack.config

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -1,12 +1,25 @@
 var React = require('react');
 
 var WeatherForm = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+
+    var location = this.refs.location.value;
+
+    if (location.length > 0) {
+      this.refs.location.value = '';
+      this.props.onSearch(location);
+    }
+  },
   render: function() {
     return (
       <div>
-        <form>
-          <input type='text' placeholder='Enter city name'/>
+        <form onSubmit={this.onFormSubmit}>
+          <div>
+            <input type="text" ref="location" placeholder='Enter city name'/>
           <button>Get Weather</button>
+          </div>
+
         </form>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     path: __dirname,
     filename: './public/bundle.js',
   },
+
   resolve: {
     root: __dirname,
     alias: {
