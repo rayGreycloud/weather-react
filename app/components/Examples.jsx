b612a1e10759de52a8ff4b029077ff1cb861cc85
rayGreycloud <rayGreycloud@gmail.com>
2016-06-08 20:13:15 -0700

    style main component and examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // var Examples = React.createClass({
 //   render: function() {
 //     return (
@@ -11,8 +11,16 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component</h3>
-      <p>Welcome to the examples page</p>
+      <h1 className='text-centered'>Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Portland'>Portland, OR</Link>
+        </li>
+        <li>
+          <Link to='/?location=Miami'>Miami, FL</Link>
+        </li>
+      </ol>
     </div>
 
   )
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -17,8 +17,11 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2> Main Component </h2>
-      {props.children}
+      <div className='row'>
+        <div className='medium-6 large-4 columns small-centered'>
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 };
