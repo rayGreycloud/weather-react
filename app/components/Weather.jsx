6bb2f643d7b1e02baa8b424c19bf6d5275d7a497
rayGreycloud <rayGreycloud@gmail.com>
2017-08-14 10:48:13 -0700

    Add forecast to weather card

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -8,7 +8,7 @@ var Weather = React.createClass({
   getInitialState: function() {
     return {
       isLoading: false
-    }
+    };
   },
   handleSearch: function(location) {
     var that = this;
@@ -20,126 +20,181 @@ var Weather = React.createClass({
       data: undefined
     });
 
-    yahooWeather.getWeather(location).then(function(data) {
-      console.log(data);
+    yahooWeather.getWeather(location).then(
+      function(data) {
+        console.log(data);
 
-      const { location, wind, atmosphere, astronomy } = data.query.results.channel;
-      const { condition, forecast } = data.query.results.channel.item;
+        const {
+          location,
+          wind,
+          atmosphere,
+          astronomy
+        } = data.query.results.channel;
+        const { condition, forecast } = data.query.results.channel.item;
 
-      function getWeatherIcon(code) {
-        var icon = '';
-        switch(code) {
-          case '0': // icon  = 'tornado';
-          case '1': // icon = 'storm-showers';
-          case '2': // icon = 'tornado';
-          case '3': // icon = 'thunderstorm';
-          case '4': icon = 'thunderstorm';
-            break;
-          case '5': icon = 'snow';
-            break;
-          case '6':
-          case '7':
-          case '8': icon = 'sleet';
-            break;
-          case '9': icon = 'cloudy-scattered-showers';
-            break;
-          case '10': icon = 'hail';
-            break;
-          case '11':
-          case '12': icon = 'showers';
-            break;
-          case '13':
-          case '14':
-          case '15':
-          case '16': icon = 'snow';
-            break;
-          case '17':
-          case '18': icon = 'hail';
-            break;
-          case '19': icon =  'cloudy';
-            break;
-          case '20':
-          case '21':
-          case '22': icon = 'fog';
-            break;
-          case '23':
-          case '24': icon = 'windy';
-            break;
-          case '25': icon = 'clear';
-            break;
-          case '26': icon = 'cloudy';
-            break;
-          case '27': icon = 'cloudy_s_sunny';
-            break;
-          case '28': icon = 'cloudy_s_sunny';
-            break;
-          case '29': icon = 'partly-cloudy';
-            break;
-          case '30': icon = 'partly-cloudy';
-            break;
-          case '31':
-          case '32':
-          case '33':
-          case '34': icon = 'clear';
-            break;
-          case '35': icon = 'hail';
-            break;
-          case '36': icon = 'clear';
-            break;
-          case '37':
-          case '38':
-          case '39': icon = 'thunderstorm';
-            break;
-          case '40': icon = 'cloudy-scattered-showers';
-            break;
-          case '41':
-          case '42':
-          case '43': icon = 'snow';
-            break;
-          case '44': icon = 'partly-cloudy';
-            break;
-          case '45': icon = 'thunderstorm';
-            break;
-          case '46': icon = 'snow';
-            break;
-          case '47': icon = 'thunderstorm';
-            break;
-          case '3200': icon = 'cloudy';
-            break;
-          default: icon = 'cloudy';
-            break;
+        function getWeatherIcon(code) {
+          var icon = '';
+          switch (code) {
+            case '0': // icon  = 'tornado';
+            case '1': // icon = 'storm-showers';
+            case '2': // icon = 'tornado';
+            case '3': // icon = 'thunderstorm';
+            case '4':
+              icon = 'thunderstorm';
+              break;
+            case '5':
+              icon = 'snow';
+              break;
+            case '6':
+            case '7':
+            case '8':
+              icon = 'sleet';
+              break;
+            case '9':
+              icon = 'cloudy-scattered-showers';
+              break;
+            case '10':
+              icon = 'hail';
+              break;
+            case '11':
+            case '12':
+              icon = 'showers';
+              break;
+            case '13':
+            case '14':
+            case '15':
+            case '16':
+              icon = 'snow';
+              break;
+            case '17':
+            case '18':
+              icon = 'hail';
+              break;
+            case '19':
+              icon = 'cloudy';
+              break;
+            case '20':
+            case '21':
+            case '22':
+              icon = 'fog';
+              break;
+            case '23':
+            case '24':
+              icon = 'windy';
+              break;
+            case '25':
+              icon = 'clear';
+              break;
+            case '26':
+              icon = 'cloudy';
+              break;
+            case '27':
+              icon = 'cloudy_s_sunny';
+              break;
+            case '28':
+              icon = 'cloudy_s_sunny';
+              break;
+            case '29':
+              icon = 'partly-cloudy';
+              break;
+            case '30':
+              icon = 'partly-cloudy';
+              break;
+            case '31':
+            case '32':
+            case '33':
+            case '34':
+              icon = 'clear';
+              break;
+            case '35':
+              icon = 'hail';
+              break;
+            case '36':
+              icon = 'clear';
+              break;
+            case '37':
+            case '38':
+            case '39':
+              icon = 'thunderstorm';
+              break;
+            case '40':
+              icon = 'cloudy-scattered-showers';
+              break;
+            case '41':
+            case '42':
+            case '43':
+              icon = 'snow';
+              break;
+            case '44':
+              icon = 'partly-cloudy';
+              break;
+            case '45':
+              icon = 'thunderstorm';
+              break;
+            case '46':
+              icon = 'snow';
+              break;
+            case '47':
+              icon = 'thunderstorm';
+              break;
+            case '3200':
+              icon = 'cloudy';
+              break;
+            default:
+              icon = 'cloudy';
+              break;
+          }
+
+          return icon;
         }
 
-        return icon;
-      }
+        function getWindDirection(deg) {
+          const index = Math.abs(Math.round((deg - 11.25) / 22.5));
+          const cardinals = [
+            'N',
+            'NNE',
+            'NE',
+            'ENE',
+            'E',
+            'ESE',
+            'SE',
+            'SSE',
+            'S',
+            'SSW',
+            'SW',
+            'WSW',
+            'W',
+            'WNW',
+            'NW',
+            'NNW'
+          ];
+          return cardinals[index];
+        }
 
-      function getWindDirection(deg) {
-        const index = Math.abs(Math.round((deg - 11.25) / 22.5));
-        const cardinals = ["N","NNE","NE","ENE","E","ESE", "SE",  "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"];
-        return cardinals[index];
+        that.setState({
+          data: {
+            location: location.city,
+            date: condition.date,
+            description: condition.text,
+            weatherIcon: getWeatherIcon(condition.code),
+            temp: condition.temp,
+            humidity: atmosphere.humidity,
+            windSpeed: wind.speed,
+            windDirection: getWindDirection(wind.direction),
+            sunrise: astronomy.sunrise,
+            sunset: astronomy.sunset,
+            forecast
+          },
+          isLoading: false
+        });
+      },
+      function(e) {
+        that.setState({
+          isLoading: false,
+          errorMessage: e.message
+        });
       }
-
-      that.setState({
-        data: {
-          location: location.city,
-          date: condition.date,
-          description: condition.text,
-          weatherIcon: getWeatherIcon(condition.code),
-          temp: condition.temp,
-          humidity: atmosphere.humidity,
-          windSpeed: wind.speed,
-          windDirection: getWindDirection(wind.direction),
-          sunrise: astronomy.sunrise,
-          sunset: astronomy.sunset
-        },
-        isLoading: false
-      });
-    }, function(e) {
-      that.setState({
-        isLoading: false,
-        errorMessage: e.message
-      });
-    });
+    );
   },
   componentDidMount: function() {
     var location = this.props.location.query.location;
@@ -158,33 +213,31 @@ var Weather = React.createClass({
     }
   },
   render: function() {
-    var {isLoading, data, errorMessage} = this.state;
+    var { isLoading, data, errorMessage } = this.state;
 
     function renderWeather() {
       if (isLoading) {
-        return <h3 className='text-center'>Fetching weather...</h3>;
+        return <h3 className="text-center">Fetching weather...</h3>;
       } else if (data) {
-        return <WeatherCard cardData={data}/>;
+        return <WeatherCard cardData={data} />;
       }
     }
 
     function renderError() {
       if (typeof errorMessage === 'string') {
-        return(
-          <ErrorModal message={errorMessage}/>
-        )
+        return <ErrorModal message={errorMessage} />;
       }
     }
 
     return (
       <div>
-        <h1 className='text-center page-title'>Get Weather</h1>
+        <h1 className="text-center page-title">Get Weather</h1>
         <hr />
-        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherForm onSearch={this.handleSearch} />
         {renderWeather()}
         {renderError()}
       </div>
-    )
+    );
   }
 });
 
--- app/components/WeatherCard.jsx
+++ app/components/WeatherCard.jsx
@@ -1,39 +1,93 @@
 var React = require('react');
 
-var WeatherCard = ({cardData}) => {
-  var {location, date, description, weatherIcon, temp, humidity, windSpeed, windDirection, sunrise, sunset} = cardData;
+var WeatherCard = ({ cardData }) => {
+  var {
+    location,
+    date,
+    description,
+    weatherIcon,
+    temp,
+    humidity,
+    windSpeed,
+    windDirection,
+    sunrise,
+    sunset,
+    forecast
+  } = cardData;
+
+  function renderForecast(forecast) {
+    return (
+      forecast.map(day => {
+        var { code, date, day, high, low, text } = day;
+
+        return (
+          <div className="oneday" key={date}>
+            <div className="date">{`${day} ${date}`}</div>
+            <div className="icon cloudy" />
+            <div className="temp-high">
+              <span className="value">{high}</span>°
+            </div>
+            <div className="temp-low">
+              <span className="value">{low}</span>°
+            </div>
+          </div>
+        );
+      });
+    );
+  }
 
   return (
     <div className="card weather-forecast">
-      <div className="city-key"></div>
-      <div className="card-last-updated"></div>
-      <div className="location">{location}</div>
-      <div className="date">{date}</div>
-      <div className="description">{description}</div>
+      <div className="city-key"></div>>
+      <div className="card-last-updated"></div>>
+      <div className="location">
+        {location}
+      </div>
+      <div className="date">
+        {date}
+      </div>
+      <div className="description">
+        {description}
+      </div>
       <div className="current">
         <div className="visual">
-          <div className={"icon " + weatherIcon}></div>
+          <div className={'icon ' + weatherIcon}></div>
           <div className="temperature">
-            <span className="value">{temp}</span><span className="scale">°F</span>
+            <span className="value">
+              {temp}
+            </span>
+            <span className="scale">°F</span>
           </div>
-        </div>
+          </div>
         <div className="description">
           <div className="humidity">
-            <span className="value">{humidity}</span>
+            <span className="value">
+              {humidity}
+            </span>
             <span className="scale">%</span>
           </div>
           <div className="wind">
-            <span className="value">{windSpeed}</span>
+            <span className="value">
+              {windSpeed}
+            </span>
             <span className="scale">mph </span>
-            <span className="direction">{windDirection}</span>
+            <span className="direction">
+              {windDirection}
+            </span>
           </div>
-          <div className="sunrise">{sunrise}</div>
-          <div className="sunset">{sunset}</div>
+          <div className="sunrise">
+            {sunrise}
+          </div>
+          <div className="sunset">
+            {sunset}
+          </div>
         </div>
       </div>
-      <div className="future"></div>
+      <div className="future">
+        {this.renderForecast(forecast)}
+      </div>
     </div>
-  )
+  );
 };
 
 module.exports = WeatherCard;
